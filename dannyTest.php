d95b58413aec74bd461f622c1daf541e44177d39
dannypellei <100869482+dannypellei@users.noreply.github.com>
2023-04-05 10:49:20 -0400

    update hmpg

--- dannyTest.php
+++ dannyTest.php
@@ -1,6 +1,36 @@
-<?php
-    echo "hello"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ContractorConnector</title>
+  <link rel="stylesheet" href="homepage.css">
+</head>
+<body>
+  <header>
+    <div class="logo-container">
+      <img src="logo.png" alt="Logo" class="logo">
+      <h1 class="site-title">ContractorConnector</h1>
+    </div>
+    <h2 class="welcome-message">Welcome Person</h2>
+    <button class="logout-button">Logout</button>
+  </header>
+  <nav>
+  <ul class="sidebar">
+      <h3 class="logo">ContractorConnector</h3>
+      <li>Home</li>
+      <li>Profile</li>
+      <li>Info</li>
+      <li>Logout</li>
+      <li>About</li>
+    </ul>
 
+    <input type="checkbox" id="sidebar-btn" class="sidebar-btn" checked/>
+    <label for="sidebar-btn"></label>
 
-
+    <div class="content">
+      <h1>Welcome Person</h1>
+    </div>
+  </nav>
+</body>
+</html>
