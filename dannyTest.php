6e5c77e8b21574f610e3eb46ea6f3c327a847b1b
dannypellei <100869482+dannypellei@users.noreply.github.com>
2023-04-05 11:45:55 -0400

    update header

--- dannyTest.php
+++ dannyTest.php
@@ -1,3 +1,7 @@
+<?php 
+session_start();
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,28 +13,12 @@
 <body>
   <header>
     <div class="logo-container">
-      <img src="logo.png" alt="Logo" class="logo">
+      <img src="images/logo_blank.png" alt="Logo" class="logo">
       <h1 class="site-title">ContractorConnector</h1>
     </div>
-    <h2 class="welcome-message">Welcome Person</h2>
-    <button class="logout-button">Logout</button>
+    <h2 class="welcome-message">Hello <?php echo $_SESSION['FirstName']?></h2>
+    <button class="logout-button" onclick="window.location.href='logout.php'">Logout</button>
+      <!-- <a href="logout.php">Logout</a> -->
   </header>
-  <nav>
-  <ul class="sidebar">
-      <h3 class="logo">ContractorConnector</h3>
-      <li>Home</li>
-      <li>Profile</li>
-      <li>Info</li>
-      <li>Logout</li>
-      <li>About</li>
-    </ul>
-
-    <input type="checkbox" id="sidebar-btn" class="sidebar-btn" checked/>
-    <label for="sidebar-btn"></label>
-
-    <div class="content">
-      <h1>Welcome Person</h1>
-    </div>
-  </nav>
 </body>
 </html>
