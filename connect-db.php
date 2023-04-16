88e833668d2a778ed032f74c11b25438b4119333
Jared <91357852+jad7qt@users.noreply.github.com>
2023-04-16 16:16:24 -0400

    Add Comment for GCP database Connection

--- connect-db.php
+++ connect-db.php
@@ -6,6 +6,15 @@ $host = 'mysql01.cs.virginia.edu';
 $dbname = 'jad7qt_b';
 $dsn = "mysql:host=$host;dbname=$dbname";
 
+// FOR GCP DATABASE
+
+// $username = 'root';                       // or your username
+// $password = 'your-root-password';     
+// $host = 'instance-connection-name';       // e.g., 'cs4750:us-east4:db-demo'; 
+// $dbname = 'your-database-name';           // e.g., 'guestbook';
+// $dsn = "mysql:unix_socket=/cloudsql/instance-connection-name;dbname=your-database-name";
+//       e.g., "mysql:unix_socket=/cloudsql/cs4750:us-east4:db-demo;dbname=guestbook";
+
 try
 {
     $db = new PDO($dsn, $username, $password);
