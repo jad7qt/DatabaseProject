c3670b2464057f5e9c25cce18469c0cc6eae55b7
Jared <91357852+jad7qt@users.noreply.github.com>
2023-04-18 13:18:42 -0400

    Switch to GCP Database

--- connect-db.php
+++ connect-db.php
@@ -1,10 +1,17 @@
 <?php
 
-$username = 'jad7qt_b';
-$password = 'UpsornWinter2023';
-$host = 'mysql01.cs.virginia.edu';
-$dbname = 'jad7qt_b';
-$dsn = "mysql:host=$host;dbname=$dbname";
+// $username = 'jad7qt_b';
+// $password = 'UpsornWinter2023';
+// $host = 'mysql01.cs.virginia.edu';
+// $dbname = 'jad7qt_b';
+// $dsn = "mysql:host=$host;dbname=$dbname";
+
+$username = 'root';                       // or your username
+$password = 'UpsornWinter2023';     
+$host = 'contractorconnecter:us-east4:contractordb';       // e.g., 'cs4750:us-east4:db-demo'; 
+$dbname = 'cc';           // e.g., 'guestbook';
+$dsn = "mysql:unix_socket=/cloudsql/$host;dbname=$dbname";
+
 
 try
 {
