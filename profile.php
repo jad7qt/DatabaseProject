627a493296686bbe3452152d1792ec27e8930c88
Jared <91357852+jad7qt@users.noreply.github.com>
2023-04-22 23:58:07 -0400

    Add modal for adding rating

--- profile.php
+++ profile.php
@@ -5,8 +5,16 @@ session_start();
 if (isset($_SESSION['UserID']) && isset($_SESSION['Username']) ) {
     // Connect to database
     require("connect-db.php");
-    require("customer-db.php");
     require("profile-db.php");
+
+    // if ( ($_SERVER['REQUEST_METHOD'] == 'POST' ) ){
+    //     if (!empty($_POST['actionBtn']) && ($_POST['actionBtn'] == "rate"))
+    //     {
+    //         rates($_POST['CustID'], $_POST['TechID'], $_POST['rating'], $_POST['comment']);
+    //         header("Location: homepage.php");
+    //         exit();
+    //     }
+    // }
     
     $Address = array();
     $Ratings = array();
@@ -49,6 +57,10 @@ if (isset($_SESSION['UserID']) && isset($_SESSION['Username']) ) {
 <head>
 	<title>Profile</title>
 	<link rel="stylesheet" type="text/css" href="css/searchResults.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 <body>
 
@@ -154,6 +166,43 @@ TESTING: PageID = <?php echo $pageID ?>
         <?php else: ?> 
             <p>No Ratings on file</p>
         <?php endif; ?>
+         <!-- MODAL BUTTOM -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+        Rate this Technician
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+                <form name="rateForm" action="rating.php" method="post">
+                <div class="modal-body">
+                    <div class="row mb-3 mx-3">
+                        Rating (0.0-5.0):
+                        <input type="number" class="form-control" name="rating" min=0 max=5 step=0.1 required />
+                    </div>
+                    <div class="row mb-3 mx-3">
+                        Comment:
+                        <input type="text" class="form-control" name="comment" required />
+                    </div>
+                    <input type="hidden" name="TechID" value=<?php echo $_GET['id'];?> />
+                    <input type="hidden" name="CustID" value=<?php echo $_SESSION['UserID'];?> />
+                </div>
+                <div class="modal-footer">
+                    <button id="buttonCreateCustomer" type="submit" class="btn btn-primary" name="actionBtn" value="rate">Post</button>
+                </div>
+                </form>
+            </div>
+        </div>
+        </div>
+    <!-- END MODAL -->
+
     </div>
     <?php }
 
--- rating-db.php
+++ rating-db.php
@@ -1,25 +1,32 @@
 <?php 
-function rate($CustID, $TechID, $rating, $comment)
+
+function rateTech($CustID, $TechID, $rating, $comment)
 {
     global $db;
-    $query1 = "INSERT INTO Ratings VALUES(:custid, :techid, :rating, :comment)";
+    $query1 = "INSERT INTO Ratings VALUES (:custid, :techid, :rating, :comment)";
     $statement1 = $db->prepare($query1);
     $statement1->bindValue(':custid', $CustID);
     $statement1->bindValue(':techid', $TechID);
     $statement1->bindValue(':rating', $rating);
     $statement1->bindValue(':comment', $comment);
-    $statement1->execute();    
-    $statement1->closeCursor();
+    echo $query1;
+    echo $CustID;
+    echo $TechID;
+    echo $rating;
+    echo $comment;
+    // $statement1->execute();    
+    // $statement1->closeCursor();
 }
 
-function delete_rate($CustID, $TechID)
-{
-    global $db;
-    $query1 = "DELETE FROM Ratings WHERE CustomerID = :custid AND TechnicianID = :techid";
-    $statement1 = $db->prepare($query1);
-    $statement1->bindValue(':custid', $CustID);
-    $statement1->bindValue(':techid', $TechID);
-    $statement1->execute();
-    $statement1->closeCursor();
-}
+// function deleteRating($CustID, $TechID)
+// {
+//     global $db;
+//     $query1 = "DELETE FROM Ratings WHERE CustomerID = :custid AND TechnicianID = :techid";
+//     $statement1 = $db->prepare($query1);
+//     $statement1->bindValue(':custid', $CustID);
+//     $statement1->bindValue(':techid', $TechID);
+//     $statement1->execute();
+//     $statement1->closeCursor();
+// }
+
 ?>
